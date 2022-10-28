b2097644e543ec894a354ef459cb10ff410304f6
initze <ingmarnitzegmail.com>
2022-10-28 16:04:02 +0200

    added imports in init

---
+++ geehotspot/__init__.py
@@ -0,0 +1,6 @@
+from geehotspot import configs
+from geehotspot import utils_plot
+from geehotspot import utils_Landsat_SR
+from geehotspot import ms_indices
+from geehotspot import high_level_functions
+from geehotspot import utils_string
